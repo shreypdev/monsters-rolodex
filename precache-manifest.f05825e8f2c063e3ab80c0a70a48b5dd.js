0f109fc73d147e27a726009fedc85590b120c922
shrey <shreypdev@gmail.com>
2020-04-14 14:40:34 -0700

    Updates

---
+++ precache-manifest.f05825e8f2c063e3ab80c0a70a48b5dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be11cd154d319ee8fa55bb2bf655a5c1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bbaba555baccc25348d6",
+    "url": "/monsters-rolodex/static/css/main.de7432c8.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbaba555baccc25348d6",
+    "url": "/monsters-rolodex/static/js/main.50d3044a.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
